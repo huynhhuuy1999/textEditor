4edd366fddc456bf69826f452cfcd0c59589c4a4
huynhhuuy1999 <huynhhuuy1@gmail.com>
2023-09-28 16:09:42 +0700

    import docker to project

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,9 +1,15 @@
 "use client";
 // Library
-import { useLayoutEffect, useRef, useState } from "react";
+import {
+  ChangeEvent,
+  useLayoutEffect,
+  useRef,
+  useState,
+  useTransition,
+} from "react";
 // Components
 import VideoCom from "@/components/VideoCom";
-import { Button } from "antd";
+import { Button, Input } from "antd";
 // Styles
 import styles from "./styles.module.css";
 
@@ -15,6 +21,9 @@ interface ITypeVideo {
 export const Test: any = () => {
   const [num, setNum] = useState<number>(0);
   const refVideo = useRef<ITypeVideo>();
+  const [textValue, setTextValue] = useState("");
+
+  const [isPending, startTransition] = useTransition();
 
   useLayoutEffect(() => {
     if (num > 3) setNum(0);
@@ -32,6 +41,16 @@ export const Test: any = () => {
     setNum(num + 1);
   };
 
+  const onChangeText = (e: ChangeEvent<HTMLInputElement>) => {
+    setTextValue(e.target.value);
+
+    startTransition(() => {
+      setTimeout(() => {
+        console.log("debounce done");
+      }, 1000);
+    });
+  };
+
   return (
     <div>
       <VideoCom ref={refVideo} />
@@ -43,6 +62,7 @@ export const Test: any = () => {
       </Button>
       <div className={styles.textNum}>{num}</div>
       <Button onClick={increaseNumber}>Click</Button>
+      <Input value={textValue} onChange={onChangeText} />
     </div>
   );
 };
